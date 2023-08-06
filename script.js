478466f94307efe66443af7339e133ad97778c39
WiseSmil3 <86897895+WiseSmil3@users.noreply.github.com>
2023-08-06 14:40:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-window.onload = function() {
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            loadContent(this.getAttribute('href').substring(1));
+        });
+    });
+});
+
+function loadContent(section) {
+    fetch(`${section}.html`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => console.error(error));
+// Load the initial content
     loadSection('Text_and_Documentations');
     loadSection('Images_and_Illustrations');
     loadSection('Videos_and_Films');
@@ -6,14 +25,6 @@ window.onload = function() {
     loadSection('Education_and_Courses');
     loadSection('Events_and_Communities');
     loadSection('Organizations');
-    loadSection('Material_Projects_and_Products');
+    loadSection('Projects_and_Products');
     loadSection('Submit_Your_Suggestions');
-}
-
-function loadSection(sectionName) {
-    fetch(sectionName + '.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(sectionName).innerHTML = data;
-        });
 }
