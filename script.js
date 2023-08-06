ea32d9a3bc200eece573a10dd42fa465aa42c4d7
WiseSmil3 <86897895+WiseSmil3@users.noreply.github.com>
2023-08-06 14:33:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-window.onload = function() {
-    loadSection('Text_and_Documentations');
-    loadSection('Images_and_Illustrations');
-    loadSection('Videos_and_Films');
-    loadSection('Music_and_Audio');
-    loadSection('Education_and_Courses');
-    loadSection('Events_and_Communities');
-    loadSection('Organizations');
-    loadSection('Material_Projects_and_Products');
-    loadSection('Submit_Your_Suggestions');
-}
+window.addEventListener('DOMContentLoaded', (event) => {
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            loadContent(this.getAttribute('href').substring(1));
+        });
+    });
+});
 
-function loadSection(sectionName) {
-    fetch(sectionName + '.html')
+function loadContent(section) {
+    fetch(`${section}.html`)
         .then(response => response.text())
         .then(data => {
-            document.getElementById(sectionName).innerHTML = data;
-        });
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => console.error(error));
 }
+
+// Load the initial content
+loadContent('Text_and_Documentations');
